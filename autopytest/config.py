74492a6ef0157f50e0bc006a9e23d4be24e2cd68
David Rice <me@davidjrice.co.uk>
2023-10-14 00:32:01 +0100

    rename

--- autopytest/config.py
+++ autopytest/config.py
@@ -4,5 +4,5 @@ from typing import Any
 def parse_pyproject_toml(path: str) -> dict[str, Any]:
     with open(path, "rb") as f:
         pyproject_toml = tomllib.load(f)
-    config: dict[str, Any] = pyproject_toml.get("tool", {}).get("autotest", {})
+    config: dict[str, Any] = pyproject_toml.get("tool", {}).get("autopytest", {})
     return config

826411b1040462d6829b22961220fe2d1d4040c9
O4FDev <luke.lucas@ou.ac.uk>
2023-10-16 00:19:10 +0100

    Add 'init' command argument to set up configuration files
    Introduced a new command argument, 'init', to the CLI. When invoked, it automates the setup of the necessary configuration files for developers, streamlining the initial setup process and ensuring a consistent configuration across different development environments.
    
    This enhancement aims to simplify onboarding for new developers and reduce the initial setup time, promoting a smoother integration into their development workflows.

--- autopytest/autotest.py
+++ autopytest/autotest.py
@@ -3,12 +3,10 @@ import re
 import sys
 import time
 from pathlib import Path
-
 from watchdog.events import FileSystemEvent, FileSystemEventHandler
 from watchdog.observers import Observer
 from .config import parse_pyproject_toml
 from .pytest_runner import PytestRunner
-
 
 class Autotest(FileSystemEventHandler):
 
@@ -19,6 +17,12 @@ class Autotest(FileSystemEventHandler):
         self.config = parse_pyproject_toml(f"{path}/pyproject.toml")
         self.source_directories = self.config["source_directories"]
         self.test_directory = self.config["test_directory"]
+        self.pytest_args = self.config["pytest_args"]
+        self.exclude_paths = [Path(p) for p in self.config["exclude_paths"]]
+
+        log_level = getattr(log, self.config["log_level"].upper(), log.INFO)
+        log_format = self.config["log_format"]
+        log.basicConfig(format=log_format, stream=sys.stdout, level=log_level)
 
         self.source_patterns = []
         for directory in self.source_directories:
@@ -26,7 +30,6 @@ class Autotest(FileSystemEventHandler):
             self.source_patterns.append(pattern)
 
         self.test_pattern = re.escape(self.test_directory) + r".+\.py$"
-
 
     def start(self) -> None:
         self.observer.start()
@@ -43,6 +46,9 @@ class Autotest(FileSystemEventHandler):
             self.observer.join()
 
     def on_modified(self, event: FileSystemEvent) -> None:
+        if Path(event.src_path) in self.exclude_paths:
+            return
+
         for pattern in self.source_patterns:
             if re.search(pattern, event.src_path):
                 path = Path(event.src_path)
@@ -54,17 +60,13 @@ class Autotest(FileSystemEventHandler):
                             test_path_components.append(f"test_{component}")
                         else:
                             test_path_components.append(component)
-                            
+                
                 test_path = "/".join(test_path_components)
-                if Path(test_path).exists() and PytestRunner.run(test_path) == 0:
-                    PytestRunner.run(".")
+                if Path(test_path).exists() and PytestRunner.run(test_path, self.pytest_args) == 0:
+                    PytestRunner.run(".", self.pytest_args)
 
         if (
             re.search(self.test_pattern, event.src_path)
-            and PytestRunner.run(event.src_path) == 0
+            and PytestRunner.run(event.src_path, self.pytest_args) == 0
         ):
-            PytestRunner.run(".")
-
-
-
-
+            PytestRunner.run(".", self.pytest_args)
--- autopytest/config.py
+++ autopytest/config.py
@@ -5,4 +5,10 @@ def parse_pyproject_toml(path: str) -> dict[str, Any]:
     with open(path, "rb") as f:
         pyproject_toml = tomllib.load(f)
     config: dict[str, Any] = pyproject_toml.get("tool", {}).get("autopytest", {})
+    
+    config.setdefault("pytest_args", [])
+    config.setdefault("exclude_paths", [])
+    config.setdefault("log_format", "%(message)s")
+    config.setdefault("log_level", "INFO")
+    
     return config

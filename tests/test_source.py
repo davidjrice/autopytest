15f41ce5f00e5187ced71d4e12184e1d4e9baad5
David Rice <me@davidjrice.co.uk>
2023-10-28 17:02:23 +0100

    ✅ ci: passing build on windows

--- tests/test_source.py
+++ tests/test_source.py
@@ -1,3 +1,4 @@
+import platform
 from pathlib import Path
 from unittest.mock import MagicMock, PropertyMock, patch
 
@@ -11,4 +12,7 @@ from autopytest.source import Source
 )
 def test_should_build_regex_pattern(_mock: MagicMock) -> None:
     source = Source(directory="app", path=".")
-    assert source.pattern == r"^/usr/src/app/app.+\.py$"
+    if platform.system() == "Windows":
+        assert source.pattern == r"^D:/usr/src/app/app.+\.py$"
+    else:
+        assert source.pattern == r"^/usr/src/app/app.+\.py$"

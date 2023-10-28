6f6ef2f2449b9f66bbc20c7925ae5d38c69a032c
David Rice <me@davidjrice.co.uk>
2023-10-28 19:30:28 +0100

    fix complexity of test path generation

--- autopytest/file.py
+++ autopytest/file.py
@@ -1,4 +1,3 @@
-import re
 from pathlib import Path
 
 from .source import Source
@@ -18,11 +17,9 @@ class File:
         if self.source.include_in_test_path(self.path_components[0]):
             self.path_components.pop(0)
 
-        for component in self.path_components:
-            if re.search(r"\.py", component):
-                self.test_path_components.append(f"test_{component}")
-            else:
-                self.test_path_components.append(component)
+        file = self.path_components.pop()
+        self.test_path_components.extend(self.path_components)
+        self.test_path_components.append(f"test_{file}")
 
         test_path = "/".join(self.test_path_components)
         return Path(test_path)

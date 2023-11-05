ad8213be8649fa2ad2722bcdb25bfb301d343cc7
David Rice <me@davidjrice.co.uk>
2023-11-05 07:44:35 +0000

    🧪 tests: add more coverage for File class

--- tests/test_file.py
+++ tests/test_file.py
@@ -26,6 +26,18 @@ def test_should_generate_test_path_for_application(application_path: str) -> Non
     assert source_file.test_path == Path("tests/app/test_calculator.py")
 
 
+def test_should_generate_test_path_for_application_with_nested_module(
+    application_path: str,
+) -> None:
+    autotest = Autotest(application_path)
+    source_file = File(
+        path=Path("fixtures/application/app/module/calculator.py").absolute(),
+        source=autotest.sources[0],
+        test_directory=autotest.config.test_directory,
+    )
+    assert source_file.test_path == Path("tests/app/module/test_calculator.py")
+
+
 def test_should_generate_test_path_for_library(library_src_path: str) -> None:
     autotest = Autotest(library_src_path)
     source_file = File(
@@ -34,3 +46,15 @@ def test_should_generate_test_path_for_library(library_src_path: str) -> None:
         test_directory=autotest.config.test_directory,
     )
     assert source_file.test_path == Path("tests/test_calculator.py")
+
+
+def test_should_generate_test_path_for_library_with_nested_module(
+    library_src_path: str,
+) -> None:
+    autotest = Autotest(library_src_path)
+    source_file = File(
+        path=Path("fixtures/library_src/src/module/calculator.py").absolute(),
+        source=autotest.sources[0],
+        test_directory=autotest.config.test_directory,
+    )
+    assert source_file.test_path == Path("tests/module/test_calculator.py")

658dc4b88addb92272f0ef2a35d41f472b5d93b7
David Rice <me@davidjrice.co.uk>
2023-11-19 17:32:42 +0000

    build(ci): fix code coverage formatting for macOS and windows by running the formatters within the build

---
+++ scripts/coverage-format.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+# Detect the operating system
+if [[ "$MSYSTEM" == "MINGW64" ]]; then
+  os="windows"
+else
+  os=$(uname | tr '[:upper:]' '[:lower:]')
+fi
+
+# Download test reporter as a static binary
+echo "Downloading Code Climate test reporter"
+if [[ "$os" == "darwin" ]]; then
+  curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-darwin-amd64 > ./cc-test-reporter
+elif [[ "$os" == "linux" ]]; then
+  curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
+elif [[ "$os" == "windows" ]]; then
+  curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-windows-amd64 > ./cc-test-reporter.exe
+else
+  echo "Unsupported operating system: $os"
+  exit 1
+fi
+chmod +x ./cc-test-reporter
+
+# Pre-test hook
+./cc-test-reporter before-build
+
+# Format the coverage data so that Code Climate understands it
+echo "Formatting coverage data"
+./cc-test-reporter format-coverage --input-type coverage.py --debug
--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-COVERAGE_INPUT_FILE="coverage.xml"
+# COVERAGE_INPUT_FILE="coverage.xml"
 COVERAGE_OUTPUT_FILE="codeclimate.json"
 AGGREGATED_COVERAGE_FILE="coverage.total.json"
 
@@ -12,31 +12,31 @@ chmod +x ./cc-test-reporter
 # Pre-test hook
 ./cc-test-reporter before-build
 
-working_dir=$(pwd)
+# working_dir=$(pwd)
 # Format the coverage data so that Code Climate understands it
-echo "Formatting coverage data"
-for dir in coverage-*; do
-if [[ -d $dir ]]; then
-    input="$dir"/$COVERAGE_INPUT_FILE
-    output="$dir"/$COVERAGE_OUTPUT_FILE
+# echo "Formatting coverage data"
+# for dir in coverage-*; do
+# if [[ -d $dir ]]; then
+#     input="$dir"/$COVERAGE_INPUT_FILE
+#     output="$dir"/$COVERAGE_OUTPUT_FILE
 
-    # Extract the platform name from the directory name
-    platform=${dir#coverage-}
-    platform=${platform%-*}
-    python_version=${dir##*-}
+#     # Extract the platform name from the directory name
+#     platform=${dir#coverage-}
+#     platform=${platform%-*}
+#     python_version=${dir##*-}
 
-    # Set the prefix based on the platform name
-    prefix=$working_dir
-    if [[ "$platform" == "macOS" ]]; then
-      prefix="/Users/runner/work/autopytest/autopytest"
-    elif [[ "$platform" == "Windows" ]]; then
-      prefix="D:/a/autopytest/autopytest"
-    fi
+#     # Set the prefix based on the platform name
+#     prefix=$working_dir
+#     if [[ "$platform" == "macOS" ]]; then
+#       prefix="/Users/runner/work/autopytest/autopytest"
+#     elif [[ "$platform" == "Windows" ]]; then
+#       prefix="D:/a/autopytest/autopytest"
+#     fi
 
-    echo "Formatting coverage data in $input for $platform on Python $python_version"
-    ./cc-test-reporter format-coverage "$input" --input-type coverage.py  --output "$output" --prefix "$prefix" --debug
-fi
-done
+#     echo "Formatting coverage data in $input for $platform on Python $python_version"
+#     ./cc-test-reporter format-coverage "$input" --input-type coverage.py  --output "$output" --prefix "$prefix" --debug
+# fi
+# done
 
 parts=$(find . -name $COVERAGE_OUTPUT_FILE | wc -l)
 files=$(find . -name $COVERAGE_OUTPUT_FILE)
